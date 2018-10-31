bb056922baa8c4b2608e4c8f42c3e2eb495acf4d
EnoahNetzach <f.castellarin@gmail.com>
2018-10-31 16:06:01 +0100

    Recognize the image text

---
+++ Dockerfile
@@ -0,0 +1,67 @@
+FROM ubuntu:18.04
+
+WORKDIR /home/node
+
+COPY package.json package.json
+COPY yarn.lock yarn.lock
+
+RUN apt update \
+  && apt install -y curl gnupg libssl-dev
+
+RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
+  && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+RUN apt update \
+  && apt install -y \
+  yarn \
+  ca-certificates \
+  fontconfig \
+  fonts-liberation \
+  gconf-service \
+  libappindicator1 \
+  libasound2 \
+  libatk1.0-0 \
+  libc6 \
+  libcairo2 \
+  libcups2 \
+  libdbus-1-3 \
+  libexpat1 \
+  libfontconfig1 \
+  libgcc1 \
+  libgconf-2-4 \
+  libgdk-pixbuf2.0-0 \
+  libglib2.0-0 \
+  libgtk-3-0 \
+  libnspr4 \
+  libnss3 \
+  libpango-1.0-0 \
+  libpangocairo-1.0-0 \
+  libstdc++6 \
+  libx11-6 \
+  libx11-xcb1 \
+  libxcb1 \
+  libxcomposite1 \
+  libxcursor1 \
+  libxdamage1 \
+  libxext6 \
+  libxfixes3 \
+  libxi6 \
+  libxrandr2 \
+  libxrender1 \
+  libxss1 \
+  libxtst6 \
+  locales \
+  lsb-release \
+  unzip \
+  wget \
+  xdg-utils
+
+RUN apt install -y \
+  tesseract-ocr \
+  tesseract-ocr-ita
+
+RUN yarn install --frozen-lockfile --production
+
+COPY index.js index.js
+
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const { execSync } = require('child_process')
+const fs = require('fs')
+const fetch = require('node-fetch')
+const { tmpdir } = require('os')
+const path = require('path')
 const puppeteer = require('puppeteer')
+const url = require('url')
 
 const run = async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
   const page = await browser.newPage()
   await page.goto('https://www.facebook.com/pg/ilbucatinocongiardino/photos/?ref=page_internal')
   await page.waitFor('[role="presentation"] a[rel="theater"]')
@@ -15,16 +21,64 @@ const run = async () => {
       const urlPage = await browser.newPage()
       await urlPage.goto(url)
       await urlPage.waitFor('a[rel="theater"] img')
-      const src = await urlPage.$eval('a[rel="theater"] img', img => img.getAttribute('src'))
+      await urlPage.click('a[rel="theater"] img')
+      await urlPage.waitFor('img.spotlight')
+      const src = await urlPage.$eval('img.spotlight', img => img.getAttribute('src'))
 
       await urlPage.close()
 
       return src
     }),
   )
-  console.log(images.join('\n'))
 
   await browser.close()
+
+  const paths = await Promise.all(
+    images.map(async image => {
+      const res = await fetch(image)
+      const filename = path.basename(url.parse(image).pathname)
+      const filepath = path.join(tmpdir(), filename)
+
+      fs.writeFileSync(filepath, await res.buffer())
+
+      return filepath
+    }),
+  )
+
+  for (const image of paths) {
+    const parsed = path.parse(image)
+    const out = path.join(parsed.dir, parsed.name)
+    const outfile = `${out}.txt`
+
+    execSync(`tesseract -l ita ${image} ${out}`, { stdio: 'ignore' })
+
+    const content = fs.readFileSync(outfile).toString()
+
+    fs.unlinkSync(outfile)
+
+    if (
+      ['bucatino', 'giardino', 'menu', 'primi', 'secondi', 'pizza', 'seguici'].some(keyword =>
+        content.toLowerCase().includes(keyword),
+      )
+    ) {
+      console.log(content)
+      break
+    }
+  }
+
+  paths.forEach(image => fs.unlinkSync(image))
 }
 
 run()
+
+process.on('unhandledRejection', err => {
+  console.error(err)
+
+  process.exit(1)
+})
+
+process.on('unhandledPromiseRejection', err => {
+  console.error(err)
+
+  process.exit(1)
+})
